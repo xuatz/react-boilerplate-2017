5bed4882f12a17c1f7d5c554a11ee80a9c7c7b9e
xuatz <xzlow10@gmail.com>
2017-06-15 01:52:32 +0800

    update mongoose definition, changed to bcryptjs

--- src/server/models/mongoose.js
+++ src/server/models/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = require("bluebird");
 let dbConnectionString = "mongodb://";
 dbConnectionString += process.env.DB_HOST || "localhost";
 if (process.env.DB_PORT) {
-	dbConnectionString += ":" + process.env.DB_PORT;
+    dbConnectionString += ":" + process.env.DB_PORT;
 }
 dbConnectionString += "/" + (process.env.DB_NAME || "placeholder_db");
 
@@ -13,34 +13,37 @@ dbConnectionString += "/" + (process.env.DB_NAME || "placeholder_db");
 // console.log("dbConnectionString", dbConnectionString);
 
 let options = Object.assign(
-	{
-		server: {
-			poolSize: 5,
-			auto_reconnect: true,
-			reconnectTries: 3,
-			socketOptions: {
-				keepAlive: 100,
-				connectTimeoutMS: 5000,
-				socketTimeoutMS: 30000
-			}
-		}
-	},
-	{
-		// user: process.env.DB_USER,
-		// pass: process.env.DB_PASSWORD
-	}
+    {
+        server: {
+            poolSize: 5,
+            auto_reconnect: true,
+            reconnectTries: 3,
+            socketOptions: {
+                keepAlive: 100,
+                connectTimeoutMS: 5000,
+                socketTimeoutMS: 30000
+            }
+        }
+    },
+    {
+        // user: process.env.DB_USER,
+        // pass: process.env.DB_PASSWORD
+    }
 );
 
 mongoose.connect(dbConnectionString, options);
 
 mongoose.connection.on("error", function(error) {
-	console.log("mongoose err", error);
+    console.log("mongoose err", error);
 });
 mongoose.connection.on("connected", function() {
-	console.log("Connection established to MongoDB");
+    console.log("Connection established to MongoDB");
 });
 mongoose.connection.on("reconnected", function() {
-	console.log("Reconnected to MongoDB");
+    console.log("Reconnected to MongoDB");
 });
 
-module.exports = mongoose;
+module.exports = {
+    mongoose,
+    uri: dbConnectionString
+};
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 if (!process.env.NODE_ENV) {
-	require("dotenv").config();
+    require("dotenv").config();
 }
 
 const express = require("express");
@@ -9,31 +9,32 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const { uri } = require("./models/mongoose.js");
 const User = require("./models/user");
 
 let corsOptions = {
-	credentials: true,
-	origin: ["http://localhost:3000"],
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	preflightContinue: true
+    credentials: true,
+    origin: ["http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: true
 };
 
 //==============================================================
 
 passport.serializeUser(function(user, cb) {
-	// console.log("serializeUser()");
-	cb(null, user.id);
+    // console.log("serializeUser()");
+    cb(null, user.id);
 });
 
 passport.deserializeUser(function(id, cb) {
-	// console.log("deserializeUser()");
-	User.findById(id, (err, user) => {
-		if (err) {
-			return cb(err);
-		}
-		return cb(null, user);
-	});
+    // console.log("deserializeUser()");
+    User.findById(id, (err, user) => {
+        if (err) {
+            return cb(err);
+        }
+        return cb(null, user);
+    });
 });
 
 //==============================================================
@@ -43,26 +44,19 @@ let app = express();
 
 //==============================================================
 
-let dbConnectionString = "mongodb://";
-dbConnectionString += process.env.DB_HOST || "localhost";
-if (process.env.DB_PORT) {
-	dbConnectionString += ":" + process.env.DB_PORT;
-}
-dbConnectionString += "/" + (process.env.DB_NAME || "placeholder_db");
-
 let sessionOptions = {
-	store: new MongoDBStore({
-		uri: dbConnectionString,
-		collection: "sessions"
-	}),
-	secret: "keyboard cat",
-	resave: true,
-	saveUninitialized: true,
-	cookie: {}
+    store: new MongoDBStore({
+        uri,
+        collection: "sessions"
+    }),
+    secret: "truly a secretive secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
 };
 
 if (app.get("env") === "production") {
-	sessionOptions.cookie.secure = true; // serve secure cookies
+    sessionOptions.cookie.secure = true; // serve secure cookies
 }
 
 //==============================================================
@@ -80,26 +74,22 @@ app.use(passport.session());
 //==============================================================
 
 app.all("*", (req, res, next) => {
-	console.log(req.method + " " + req.url);
-	next();
+    console.log(req.method + " " + req.url);
+    next();
 });
 
-// app.use((req, res, next) => {
-// 	if (req.method === 'OPTIONS') {
-// 		next();
-// 	} else {
-// 		next();
-// 	}
-// });
-
-// app.get("/", (req, res) => {
-// 	res.send("hi guys");
-// });
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+        next();
+    } else {
+        next();
+    }
+});
 
 //==============================================================
 
 app.use("/auth", require("./api/auth.js"));
 
 app.listen(9000, function() {
-	console.log("Example app listening on port 9000!");
+    console.log("Example app listening on port 9000!");
 });
